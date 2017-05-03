9bc9537c0aaec6589bed491faf64693474c685f0
dpilawa <dpilawa@gmail.com>
2017-05-04 00:37:04 +0200

    unit cube initialization fixed

--- cilantro/MeshObject.cpp
+++ cilantro/MeshObject.cpp
@@ -21,18 +21,20 @@ void MeshObject::Clear ()
 void MeshObject::InitUnitCube ()
 {
 	Clear ();
-	// fixme - winding
+
 	vertices.push_back (Vector4f (.5f, .5f, .5f));
 	vertices.push_back (Vector4f (-.5f, .5f, .5f));
-	vertices.push_back (Vector4f (.5f, -.5f, .5f));
 	vertices.push_back (Vector4f (-.5f, -.5f, .5f));
+	vertices.push_back (Vector4f (.5f, -.5f, .5f));
 
 	vertices.push_back (Vector4f (.5f, .5f, -.5f));
 	vertices.push_back (Vector4f (-.5f, .5f, -.5f));
-	vertices.push_back (Vector4f (.5f, -.5f, -.5f));
 	vertices.push_back (Vector4f (-.5f, -.5f, -.5f));
+	vertices.push_back (Vector4f (.5f, -.5f, -.5f));
 
-	faces = std::vector<int> { 0, 1, 2, 3, 5, 6 };
+	faces = std::vector<int> { 0, 1, 2, 0, 2, 3, 6, 5, 4, 4, 7, 6,		// front, back
+							   0, 4, 1, 1, 4, 5, 3, 2, 6, 3, 6, 7,		// top, bottom
+							   1, 5, 6, 1, 6, 2, 0, 3, 4, 3, 7, 4 };	// left, right
 }
 
 // Initialize mesh in local space as unit plane parallel to x and z axis, with center in space origin
