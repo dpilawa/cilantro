04c95fbfd2d4b9e6e63848ccb4e59d4c39f55bf1
dpilawa <dpilawa@gmail.com>
2021-03-14 20:14:14 +0100

    added Mesh object

---
+++ cilantro/include/resource/Mesh.h
@@ -0,0 +1,12 @@
+#ifndef _TEXTURE_H_
+#define _TEXTURE_H_
+
+#include "cilantroengine.h"
+#include "resource/LoadableResource.h"
+
+class Mesh : public LoadableResource
+{
+
+};
+
+#endif
--- cilantro/include/resource/Resource.h
+++ cilantro/include/resource/Resource.h
@@ -5,7 +5,6 @@
 #include "resource/ResourceManager.h"
 #include <string>
 
-
 class Resource
 {
     template <typename Base>
